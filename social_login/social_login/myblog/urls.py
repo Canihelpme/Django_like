eff3666b6a1b89d2422d86b5d370e7365f653cb5
Canihelpme <shindh0429@gmail.com>
2020-09-11 22:10:01 +0900

    commit

--- social_login/social_login/myblog/urls.py
+++ social_login/social_login/myblog/urls.py
@@ -13,10 +13,11 @@ urlpatterns = [
     ## 프로젝트의 urls 와의 차이점 - App 내의 view 인지 명시하는 부분이 빠짐
     path('', views.home, name='home'),
     path('<int:post_id>/', views.detail, name="detail"),
-    path('/new', views.new, name="new"),
+    path('new/', views.new, name="new"),
     path('<int:post_id>/edit/', views.edit, name="edit"),
     path('<int:post_id>/delete/', views.delete, name="delete"),
     path('<int:post_id>/comments/create/', views.comments_create, name='comments_create'),
     path('<int:post_id>/comments/<int:comment_id>/delete/', views.comments_delete, name='comments_delete'),
     path('<int:post_id>/comments/<int:comment_id>/update/', views.comments_update, name='comments_update'),
+    path('<int:post_id>/like/', views.like, name='like'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
